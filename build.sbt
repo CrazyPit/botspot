c91702d0b41179873b0f01935bfd14cd0e7f24b9
Vadimbhai <sfai.88@gmail.com>
2015-08-20 12:39:37 +0300

    akka-testkit added, BotSpotRunner fix

--- build.sbt
+++ build.sbt
@@ -6,7 +6,9 @@ scalaVersion := "2.11.7"
 
 mainClass := Some("BotSpot")
 
-libraryDependencies ++= Seq("org.scalatest" % "scalatest_2.11" % "2.2.4" % "test",
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "2.2.5" % "test",
+  "com.typesafe.akka" %% "akka-testkit" % "2.3.12" % "test",
   "org.json4s" %% "json4s-jackson" % "3.2.11",
   "com.typesafe" % "config" % "1.3.0",
   "org.json4s" %% "json4s-native" % "3.2.11",
--- src/main/scala/framework/BotSpotRunner.scala
+++ src/main/scala/framework/BotSpotRunner.scala
@@ -8,8 +8,6 @@ import com.typesafe.config.{Config, ConfigFactory}
 
 //import botspot.AnonymousChatBotAsync
 
-import botspot.api.models.BotConfig
-
 
 /**
  * Created by petrrezikov on 15.08.15.
@@ -21,12 +19,11 @@ class BotSpotRunner(val botName: String, configFileName: String,
   def run(): Unit = {
     // TODO: add error checking
     val config = ConfigFactory.parseFile(new File(configFileName))
-    val botConfig = BotConfig(config.getInt(s"$botName.bot.id"), config.getString(s"$botName.bot.token"))
     val system = ActorSystem(botName, ConfigFactory.parseString( """
                                           telegram-http-dispatcher {
                                             mailbox-type = "akka.contrib.mailbox.PeekMailboxType"
                                             max-retries = 5
                                           }"""))
-    system.actorOf(BotRunner.props(botConfig, config.getConfig(botName), botLogicFactoryFunc), "controller")
+    system.actorOf(BotRunner.props(config.getConfig(botName), botLogicFactoryFunc), "controller")
   }
 }
